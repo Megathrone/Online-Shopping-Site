c300bdade6a93ff9175edd8870a67e36819af1cf
Megathrone <yizhouwang418@gmail.com>
2018-03-13 12:59:03 +0800

    add json

--- tmall_ssm/src/main/java/com/megathrone/tmall/controller/CategoryController.java
+++ tmall_ssm/src/main/java/com/megathrone/tmall/controller/CategoryController.java
@@ -1,22 +1,30 @@
 package com.megathrone.tmall.controller;
 
 import com.megathrone.tmall.pojo.Category;
+import com.megathrone.tmall.pojo.Students;
 import com.megathrone.tmall.service.CategoryService;
 import com.megathrone.tmall.util.ImageUtil;
 import com.megathrone.tmall.util.Page;
 import com.megathrone.tmall.util.UploadedImageFile;
+import org.apache.ibatis.annotations.ResultMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("")
@@ -85,5 +93,11 @@ public class CategoryController {
             ImageIO.write(img, "jpg", file);
         }
         return "redirect:/admin_category_list";
+    }
+
+    @RequestMapping(value = "hello",method = RequestMethod.GET)
+    @ResponseBody
+    public Students foo(){
+        return new Students("zhouy",23);
     }
 }
---
+++ tmall_ssm/src/main/java/com/megathrone/tmall/pojo/Students.java
@@ -0,0 +1,27 @@
+package com.megathrone.tmall.pojo;
+
+public class Students {
+    private String name;
+    private int age;
+
+    public Students(String name,int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
