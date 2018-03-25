a49c8cfdcbcd86c07d6c1c87244e1837a70d5d4b
Megathrone <yizhouwang418@gmail.com>
2018-03-25 11:49:28 +0800

    1

--- tmall_ssm/src/main/java/com/megathrone/tmall/controller/ForeController.java
+++ tmall_ssm/src/main/java/com/megathrone/tmall/controller/ForeController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -195,6 +196,24 @@ public class ForeController {
         }
         return "redirect:forebuy?oiid=" + oiid;
     }
+
+    @RequestMapping("forebuy")
+    public String buy(Model model, String[] oiid, HttpSession session) {
+        List<OrderItem> ois = new ArrayList<>();
+        float total = 0;
+
+        for (String strid : oiid) {
+            int id = Integer.parseInt(strid);
+            OrderItem oi = orderItemService.get(id);
+            total += oi.getProduct().getPromotePrice() * oi.getNumber();
+            ois.add(oi);
+        }
+
+        session.setAttribute("ois", ois);
+        model.addAttribute("total", total);
+        return "fore/buy";
+    }
+
 
 }
 
